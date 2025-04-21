fc263214e97206e3d6dfbe20b6147b22f25fc9c1
Yifan Gao <git@yfgao.com>
2025-04-22 02:49:24 +0800

    Update logging level in main application to INFO for improved clarity

--- src/__main__.py
+++ src/__main__.py
@@ -14,7 +14,7 @@ from src.mqtt.client import AsyncMQTTClient
 
 # Set up logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
